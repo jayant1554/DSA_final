82cfb577a80d9a616d39d044d66f7fbaa2d34b88
jayant1554 <jk913600@gmail.com>
2025-03-22 23:34:14 +0530

    Update leetcode2239.py

---
+++ GREG/ARRAY/leetcode2239.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/find-closest-number-to-zero/description/
+#find the closest number to zero
+from typing import List
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if abs(i)<abs(closest):
+                closest=i
+        if closest<0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
