89699080113a58fa298ed4c93589841933eab25a
jayant1554 <jk913600@gmail.com>
2025-04-01 23:01:01 +0530

    Update leetcode2239.py

--- GREG/ARRAY/leetcode2239.py
+++ GREG/ARRAY/leetcode2239.py
@@ -3,6 +3,7 @@
 from typing import List
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
+        closest=nums[0]
         for i in nums:
             if abs(i)<abs(closest):
                 closest=i
